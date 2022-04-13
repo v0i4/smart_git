208785938f41104e1250f249871afda6207dc4f3
v0i4 <73661148+v0i4@users.noreply.github.com>
2022-04-12 23:40:37 -0300

    improving tests

--- lib/smart_git_web/router.ex
+++ lib/smart_git_web/router.ex
@@ -10,12 +10,14 @@ defmodule SmartGitWeb.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
+  #  pipeline :api do
+  #    plug :accepts, ["json"]
+  #  end
 
   scope "/", SmartGitWeb do
     pipe_through :browser
+
+    live "/", PageLive, :index
   end
 
   # Other scopes may use custom stacks.
