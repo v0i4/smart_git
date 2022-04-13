db82a76e73a58d87df82e2e515e92bb572aef4be
v0i4 <73661148+v0i4@users.noreply.github.com>
2022-04-13 18:11:34 -0300

    preparing frontend

--- test/live/page/page_live_test.exs
+++ test/live/page/page_live_test.exs
@@ -6,6 +6,10 @@ defmodule SmartGitWeb.PageLiveTest do
   test "load page elements", %{conn: conn} do
     {:ok, view, _html} = live(conn, Routes.page_path(conn, :index))
 
-    assert has_element?(view, "[data-role=btn-language-select][data-id=elixir]", "Elixir")
+    assert has_element?(view, "[data-role=btn-language-select][data-id=elixir]", "elixir")
+    assert has_element?(view, "[data-role=btn-language-select][data-id=javascript]", "javascript")
+    assert has_element?(view, "[data-role=btn-language-select][data-id=golang]", "golang")
+    assert has_element?(view, "[data-role=btn-language-select][data-id=rust]", "rust")
+    assert has_element?(view, "[data-role=btn-language-select][data-id=erlang]", "erlang")
   end
 end
