7f107c3a78e0b93bb817e5477bdda32c5f0eed31
v0i4 <73661148+v0i4@users.noreply.github.com>
2022-04-15 15:40:04 -0300

    insert repos in database

--- lib/smart_git_web/live/page/repo_detail/repo_detail.ex
+++ lib/smart_git_web/live/page/repo_detail/repo_detail.ex
@@ -1,11 +1,14 @@
 defmodule SmartGitWeb.Page.RepoDetail do
   use SmartGitWeb, :live_component
+  alias SmartGit.GitRepos
 
   def update(assigns, socket) do
     {:ok, socket |> assign(assigns) |> assign(message: nil)}
   end
 
   def handle_event("add-repo", _params, socket) do
+    repo = socket.assigns.repo
+    GitRepos.create(repo)
     message = (socket.assigns.message == nil && "repo added!") || nil
     {:noreply, assign(socket, message: message)}
   end
